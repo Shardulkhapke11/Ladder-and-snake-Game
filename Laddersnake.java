61c2b5004ecbfd40eac1d02fe232d3a99aa78235
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-17 01:54:13 +0530

    Added: UC2-Die

--- Laddersnake.java
+++ Laddersnake.java
@@ -1,9 +1,12 @@
 public class Laddersnake {
 public static void main(String[] args){
 	int Position=0;
-	
-	
 System.out.println("Welcome to Snake & Ladder Program Problem");
-System.Out.println("Your at Starting Position"+Position);
+System.out.println("Your at Starting Position"+Position);
+Dice();
+}
+public static void Dice(){
+	int Dice = (int)Math.floor(Math.random()*10)%6+1;
+	System.out.println("Dice Value is "+Dice);
 }
 }
