07e7038280d5d20b44c77a5ba589e5b9d520c731
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-17 01:47:38 +0530

    Added: UC1-Position-0

--- Laddersnake.java
+++ Laddersnake.java
@@ -1,5 +1,9 @@
 public class Laddersnake {
-public static void main(String[] args){	
+public static void main(String[] args){
+	int Position=0;
+	
+	
 System.out.println("Welcome to Snake & Ladder Program Problem");
+System.Out.println("Your at Starting Position"+Position);
 }
 }
