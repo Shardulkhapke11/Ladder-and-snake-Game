c31790c9365289654c175bc6f3c07b4166236945
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-17 03:47:49 +0530

    Added: UC6-position

--- Laddersnake.java
+++ Laddersnake.java
@@ -1,18 +1,19 @@
 public class Laddersnake{
-public static void main(String[] args){	
+public static void main(String[] args){
+	int count = 0;
 int Player = 0;
   while(Player<=99){
-	System.out.println("Player is at " + Player);
+	System.out.println("Player is at" + Player);
 	int Die=(int) (Math.random() * 10) % 6+1;
-		System.out.println("Die value is " + Die);
+		count =(count + 1);
+		System.out.println("Die value is at" + Die);
 	int Condition=(int) (Math.random() * 10) % 3+1;
 	//System.out.println("case number is " + Condition);
 	 switch(Condition) {
 	   case 1:
-	   System.out.println( "No Play You in same Player" );
+	   System.out.println( "No Play You are in the same Player" );
 	   break;
-	  
-	   case 2:
+	  case 2:
 	   Player = (Player + Die);
 	   if(Player>100){
 		   Player = (Player - Die);
@@ -20,16 +21,16 @@ int Player = 0;
 	   }
 	   System.out.println( "After ladder Player is at :" + Player );
 	   break;
-	   
 	   case 3: 
 	   Player = (Player - Die);
 	  
 	  if(Player<0){
 		  Player = 0;
 	  }
-		 System.out.println( "After snake Player is at" + Player );
+		 System.out.println( "After snake Player is at :" + Player );
 	   break;
 	   }
 	    }
+		System.out.println( "Die play number " + count );
 	     }
 		  }
